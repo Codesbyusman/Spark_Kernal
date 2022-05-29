37a12c03dcd28b62c36e263b2c11aebbbf814eac
codesbyusman <codesbyusman@gmail.com>
2022-05-29 12:34:50 +0500

    banner

--- code/os_kernal.cpp
+++ code/os_kernal.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+
+    // main thread is the controller thread
+    // creating other on specified
+    if (argc > 1)
+    {
+    }
+    else
+    {
+        cout << "\n\t\t ::::::::: Please Specify the Parameters ::::::::\n"
+             << endl;
+        cout << "\n <objectFile> <inputFile> <#of cpu> <schedulingPolicy> <timeSlice> <outputFile>\n"
+             << endl;
+        cout << "\t\t ::::::::: Where Scheduling Plicy as follow :::::::: \n"
+             << endl;
+        cout << "\t\t f for first come first serve"
+             << endl;
+        cout << "\t\t r for round robbin  and timeslice for it only "
+             << endl;
+        cout << "\t\t p for the pirority based scheduling \n"
+             << endl;
+    }
 
     return 0;
 }
