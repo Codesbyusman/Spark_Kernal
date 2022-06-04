389a4f490375b4e6f19f297a389771b6c63ba6db
M-ismail-ramzan <m.ismail_ramzan@yahoo.com>
2022-06-04 21:14:10 +0500

    working_A_bit

--- OS_Scheduler_Simulation/os-kernel.cpp
+++ OS_Scheduler_Simulation/os-kernel.cpp
@@ -22,12 +22,38 @@ using namespace std;
 // A PCB Struct to store the information if the processes
 //#include "os-kernel.cpp"
 pthread_mutex_t mutex_locked_thread;
+// For the Input Arguments
 int CPU_CORES = 0;
+char ALGO = 'f';
+// Basic declearation to avoid ERROR'S
 struct PCB;
-class Scheduler;
+class Processer;
+class Scheduler
+{
+
+public:
+    Processer *my_cpu;
+    // these functions are just to Run the Threads for CPU
+    void start_scheduler_with_threads(pthread_t *);
+    static void *helper_fill_the_scheduler_queue(void *p);
+    void fill_the_scheduler_queue(int cpu_cores);
+    void send_new_queue_to_Ready_queue();
+    void send_running_queue_to_waiting_queue(PCB);
+    void send_waiting_queue_to_ready_queue();
+    static void *helper_send_waiting_queue_to_ready_queue(void *p);
+};
+// Following are the Main Queues that will be used in this process
 list<PCB> qlist;
 queue<PCB> queue_new;
 queue<PCB> queue_ready;
+queue<PCB> queue_running;
+queue<PCB> queue_waiting;
+queue<PCB> queue_terminated;
+// During the Execution we will be focusing on the Following Variables
+int TOTAL_EXECUTION_TIME = 0;
+int TOTAL_CONTEXT_SWICTING = 0;
+int TOTAL_TIME_READY_STATE = 0;
+
 struct PCB
 {
     // making the varibles for PCB
@@ -139,21 +165,99 @@ struct PCB
 };
 //-------------------------------END PCB-----------------------//
 
-//-----------------------Scheduler--------------------------------//
-class Scheduler
+//------------------------------START OF PROCESSER------------------//
+class Processer
+{
+public:
+    // A Process PCB values will be sent to the Algo and the Algo will then Execute
+    // the Process
+    void Algo_First_come_First_server(PCB, Scheduler *);
+};
+// Take Pcb object and Run the Process
+void Processer::Algo_First_come_First_server(PCB pcb_obj, Scheduler *scheduler_ptr)
 {
 
-public:
-    // these functions are just to Run the Threads for CPU
-    void start_scheduler_with_threads(pthread_t*);
-    static void *helper_fill_the_scheduler_queue(void *p);
-    void fill_the_scheduler_queue(int cpu_cores);
-    void send_new_queue_to_Ready_queue();
-};
+    // Here we have to Perform the FCFS
+    cout << "\n Process " << pcb_obj.process_name << " has To be Executed in " << pcb_obj.cpu_time << " with " << pcb_obj.input_output_time << " I/O";
 
+    // Here i will be counting the Total Execution time
+
+    if (pcb_obj.input_output_time > 0)
+    {
+        // send the process to the Waiting Queue ///
+        TOTAL_CONTEXT_SWICTING++;
+        // adding 2 cuz
+        // -- CPU TIME + I/O time (Assume 1 sec)
+        //     So,    1 + 1 = 2
+        TOTAL_EXECUTION_TIME = TOTAL_EXECUTION_TIME + 2;
+        // drcreasing the i/o by one because it' going for input
+        pcb_obj.input_output_time=(pcb_obj.input_output_time)-1;
+        // just assuming it will go to i/o after 1 sec
+         pcb_obj.cpu_time=(pcb_obj.cpu_time)-1;
+        // send the Process to the waiting Queue
+        // queue_waiting.push(pcb_obj);
+        queue_running.pop();
+        queue_running.push(pcb_obj);
+        scheduler_ptr->send_running_queue_to_waiting_queue(pcb_obj);
+    }
+    else
+    {
+        TOTAL_EXECUTION_TIME = TOTAL_EXECUTION_TIME + pcb_obj.cpu_time;
+        cout << "\n -----------COMPLETED EXECUTION -------------\n";
+        cout << pcb_obj.process_name  << "\n" << "---------------------";
+    }
+}
+
+//-----------------------Scheduler--------------------------------//
+
+void Scheduler::send_running_queue_to_waiting_queue(PCB obj)
+{
+    // This function will send the running Queue to the waiting Queue
+    PCB debug = queue_running.front();
+    cout << "\n QUEUE Runnong " << debug.process_name << " " << debug.input_output_time << "   " << debug.cpu_time ;
+    queue_waiting.push(queue_running.front());
+    queue_running.pop();
+}
+void Scheduler::send_waiting_queue_to_ready_queue()
+{
+    // if waiting queue is empty then wait for something
+    //  this will be a thread working in it's own zone
+
+    // ceating a thread
+    pthread_t waiting_queue_thread_id;
+    if (pthread_create(&waiting_queue_thread_id, NULL, Scheduler::helper_send_waiting_queue_to_ready_queue, NULL) != 0)
+    {
+        perror("\n Kernel Threads are unable to create");
+        exit(0);
+    }
+
+    // cout << "**************************************************8";
+}
+
+void *Scheduler::helper_send_waiting_queue_to_ready_queue(void *p)
+{
+    while (1)
+    {
+        // if waiting queue is not empty the do the following
+        if (!queue_waiting.empty())
+        {
+            
+            cout << "\n RUNNING IN THE BACKGROUND FOR NO REASON";
+            queue_ready.push(queue_waiting.front());
+            queue_waiting.pop();
+            sleep(1);
+        }
+        else
+        {
+            sleep(1);
+        }
+    }
+    return NULL;
+}
 void Scheduler::send_new_queue_to_Ready_queue()
 {
-
+    // first thing first start the working of backgrodun
+    send_waiting_queue_to_ready_queue();
     // This function will send the new Queue to Ready according to Arrival Time
     // cout << "\n" << queue_new.front().process_name;
 
@@ -171,7 +275,7 @@ void Scheduler::send_new_queue_to_Ready_queue()
 }
 void Scheduler::start_scheduler_with_threads(pthread_t *threads_id)
 {
-  
+
     // create the corese as specified by the user
     for (int i = 0; i < CPU_CORES; i++)
     {
@@ -194,20 +298,31 @@ void *Scheduler::helper_fill_the_scheduler_queue(void *p)
 }
 void Scheduler::fill_the_scheduler_queue(int cpu_cores)
 {
-    while(1){
-    pthread_mutex_lock(&mutex_locked_thread);
-    sleep(1);
-    // locked so one thread can write to the Queue at a time
-    
-    cout << "\nThread waiting: " << pthread_self() << "\n";
-    while (queue_ready.empty());
-    cout << " \nThread is Running : " << pthread_self() << "\n";
-    queue_ready.pop();
-    // priority_queue<PCB> queue_new;
-    // Now we have to push into the queue using the arriaval time
-    // here we have to implement the Queue where the information will be transmitted
-    pthread_mutex_unlock(&mutex_locked_thread);
-    // return NULL;
+    while (1)
+    {
+        pthread_mutex_lock(&mutex_locked_thread);
+        sleep(1);
+        // locked so one thread can write to the Queue at a time
+        cout << "\nThread waiting: " << pthread_self() << "\n";
+        while (queue_ready.empty())
+            ;
+        cout << " \nThread is Running : " << pthread_self() << "\n";
+        // get the PCB and give it to the Processer
+        PCB tmp_obj = queue_ready.front();
+        queue_ready.pop();
+        if (ALGO == 'f')
+        {
+            cout << "Running the FCFS";
+            // GETTING TO THE RUNNING STATE
+            TOTAL_CONTEXT_SWICTING++;
+            queue_running.push(tmp_obj);
+            my_cpu->Algo_First_come_First_server(tmp_obj, this);
+        }
+        // priority_queue<PCB> queue_new;
+        // Now we have to push into the queue using the arriaval time
+        // here we have to implement the Queue where the information will be transmitted
+        pthread_mutex_unlock(&mutex_locked_thread);
+        // return NULL;
     }
 }
 //-------------------------------End Scheduler-----------------------//
@@ -218,8 +333,8 @@ class Kernel
     PCB pcb_arr;
     int count_pcb_entry;
     // create the array of the threads id's
-      // This is time to start the Threads...
-    
+    // This is time to start the Threads...
+
 public:
     Scheduler *kernel_scheduler;
     Kernel()
@@ -229,7 +344,7 @@ public:
         kernel_scheduler = new Scheduler;
     }
     void Implement_start(string file_name);
-    void controller_thread(int cpu_cores,pthread_t*);
+    void controller_thread(int cpu_cores, pthread_t *);
     void start_scheduler_with_threads(int cpu_cores);
     void showlist(list<PCB> g);
     void send_list_to_queue_new(list<PCB>);
@@ -263,7 +378,7 @@ void Kernel::controller_thread(int cpu_cores, pthread_t *threads_id)
     cout << "\n Controller Thread will Now Ask Scheduler (Short term) to Send the Processes to the Ready Queue According to their Arrival Time \n";
     ;
 
-       // We can either have 4,2,1 CPU's Remember THAT!
+    // We can either have 4,2,1 CPU's Remember THAT!
     kernel_scheduler->start_scheduler_with_threads(threads_id);
     kernel_scheduler->send_new_queue_to_Ready_queue();
 
@@ -312,14 +427,13 @@ void Kernel::send_list_to_queue_new(list<PCB> g)
 
 void Kernel::Implement_start(string file_name)
 {
- 
 
     cout << "\n Reading the File of the Processes \n";
     // Now I need to open that file and read all the information
     string process = "";
     // open the file
     fstream newfile;
-    newfile.open("processes.txt");
+    newfile.open(file_name);
     // now read information
     if (newfile.is_open())
     { // checking whether the file is open
@@ -357,34 +471,50 @@ int main(int argc, char *argv[])
     Kernel spark_kernal;
     cout << "\n Checking for Argumnets \n";
     // checking for the erros at the arguments
-    if (argc == 1)
+    if (argc < 5)
     {
         cout << "\n Please Use the Following Format \n";
         cout << "\nos-kernel <Input file> <# CPUs> r <timeslice> <Output file>\n";
+        cout << "\nos-kernel <Input file> <# CPUs> f <Output file>\n";
+        cout << "\nos-kernel <Input file> <# CPUs> p <Output file>\n";
     }
     else
     {
+        cout << argv[3];
+        // checking some argument..
+
+        if (!(*(argv[3]) == 'f' || *(argv[3]) == 'p' || *(argv[3]) == 'r'))
+        {
+            cout << "\n Invalid Aurgumnets \n";
+            exit(1);
+        }
+        else
+        {
+            // this is the Algo we are going to RUNnnn
+            ALGO = *(argv[3]);
+        }
         // reading the arguments
         for (int i = 0; i < argc; ++i)
         {
             // taking the Arguments
-            if (i != 0)
-                // cout << argv[i] << "\n";
 
-                // First Argument
-                if (i == 1)
-                {
-                    // First Argumnet is going to be input file
-                    spark_kernal.Implement_start(argv[i]);
-                }
+            // cout << argv[i] << "\n";
+
+            // First Argument
+            if (i == 1)
+            {
+                // First Argumnet is going to be input file
+                spark_kernal.Implement_start(argv[i]);
+            }
             // Second Argument
             if (i == 2)
             {
                 if (stoi(argv[i]) == 1 || stoi(argv[i]) == 2 || stoi(argv[i]) == 4)
                 {
+
                     CPU_CORES = stoi(argv[i]);
                     pthread_t threads_id[CPU_CORES];
-                    spark_kernal.controller_thread(stoi(argv[i]),threads_id);
+                    spark_kernal.controller_thread(stoi(argv[i]), threads_id);
 
                     // spark_kernal.start_scheduler_with_threads(stoi(argv[i]));
                 }
@@ -393,6 +523,10 @@ int main(int argc, char *argv[])
                     cout << "\n Invalid Aurgumnets \n";
                     exit(1);
                 }
+            }
+            // Second Argument
+            if (i == 3)
+            {
             }
         }
     }
